1266a5938386bab678a605e40137c1146a0a0b29
Karba <ivan.karbashevskyi@gmail.com>
2025-06-28 17:58:56 +0200

    Add object pool implementations for various solutions

---
+++ week-3/object-pool/(solution of nr.1) 4-bad.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const poolify = ({ factory, factoryArguments = [], size, max }) => {
+  const pool = Array.from({ length: size }, () => factory(...factoryArguments));
+
+  const acquire = () => pool.pop() || factory(...factoryArguments);
+
+  const release = (instance) => {
+    const status = pool.length < max && !pool.includes(instance);
+    if (status) {
+      pool.push(instance);
+    }
+    return status;
+  };
+
+  return { acquire, release };
+};
+
+
+// Usage
+
+const FILE_BUFFER_SIZE = 4096;
+const createBuffer = (FILE_BUFFER_SIZE) => new Uint8Array(FILE_BUFFER_SIZE);
+
+const pool = poolify({ 
+    factory: createBuffer, 
+    factoryArguments: [FILE_BUFFER_SIZE], 
+    size: 10, 
+    max: 15, 
+});
+
+const buf = pool.acquire();
+console.log({ buf });
+
+pool.release(buf);
---
+++ week-3/object-pool/(solution of nr.6).js
@@ -0,0 +1,40 @@
+'use strict';
+
+class Pool {
+  constructor({ factory, size, max }) {
+    this.factory = factory;
+    this.max = max;
+    this.instances = Array.from({ length: size }, this.factory);
+    this.queue = [];
+  }
+
+  acquire() {
+    return new Promise((resolve) => {
+      const instance = this.instances.pop();
+      if (instance) {
+        resolve(instance);
+      } else {
+        this.queue.push(resolve);
+      }
+    });
+  }
+
+  release(instance) {
+    if (this.queue.length > 0) {
+      const resolve = this.queue.shift();
+      resolve(instance);
+    } else if (this.instances.length <= this.max) {
+      this.instances.push(instance);
+    }
+  }
+}
+
+// Usage
+const createBuffer = (size) => new Uint8Array(size);
+const FILE_BUFFER_SIZE = 4096;
+const createFileBuffer = () => createBuffer(FILE_BUFFER_SIZE);
+const pool = new Pool({ factory: createFileBuffer, size: 10, max: 15 });
+pool.acquire().then((instance) => {
+  console.log({ instance });
+  pool.release(instance);
+});
